be0ef1bfd3c20ba8df3b092ef0eafce4e96ebd4c
Erik Ramirez <iam.erikrz@gmail.com>
2025-11-11 16:43:39 -0600

    feat(main): add song parsing after fetch
    - Initialize Parser after fetching page with Fetcher.
    - Parse songs and print results.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from config import URL, HEADLESS
 from scraper.browser_manager import BrowserManager
 from scraper.fetcher import Fetcher
+from scraper.processor import Parser
 
 
 with BrowserManager(headless=HEADLESS) as bm:
     fetcher = Fetcher(browser=bm)
     fetcher.fetch(URL, scroll=True)
+
+    parser = Parser(browser=bm)
+    songs = parser.parse_songs()
+    print(songs)
