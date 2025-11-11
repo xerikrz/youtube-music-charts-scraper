cf8c4602809179a1c028158a62de61596ee59275
Erik Ramirez <iam.erikrz@gmail.com>
2025-11-11 16:03:11 -0600

    feat(config): update browser settings and add scraping configuration
    - Enable HEADLESS mode for UI-less execution.
    - Add DATE constant to record the execution date.
    - Define target URL for scraping.
    - Add wait parameters (DEFAULT_WAIT, RANDOMIZE, RANDOM_WAIT_MIN, RANDOM_WAIT_MAX).
    - Include scroll values (SCROLL_PAUSE, SCROLL_STEP, SCROLL_DELAY, SCROLL_TOP_DELAY).

--- src/config.py
+++ src/config.py
@@ -10,12 +10,37 @@ DATA_DIR = BASE_DIR / "data"
 DATA_DIR.mkdir(exist_ok=True)
 
 # ============================
+# Execution date
+# ============================
+DATE = f"{datetime.now().date()}"
+
+# ============================
 # Browser settings
 # ============================
-HEADLESS = False 
+HEADLESS = True
 
 VIEWPORT = {
     "width": 1280,
     "height": 800,
 }
 
+# ============================
+# Scraping settings
+# ============================
+URL = "https://charts.youtube.com/charts/TopSongs/global/weekly"
+
+# Basic wait
+DEFAULT_WAIT = 3000
+RANDOMIZE = True
+RANDOM_WAIT_MIN = 300
+RANDOM_WAIT_MAX = 500
+
+# scroll_to_bottom
+SCROLL_PAUSE = 500
+
+# auto_scroll
+SCROLL_STEP = 300
+SCROLL_DELAY = 500
+
+# scroll_to_top
+SCROLL_TOP_DELAY = 300
